ea0b5cd2383ca8a30d881425e53548cda9da0441
Nyame-Wolf <mumenyam@gmail.com>
2022-11-15 02:20:15 +0300

    add categories scaffold
    add and run categoreies migration
    
    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "splash_screen#index"
+  resources :categories
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root "splash_screen#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_180135) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_194002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,6 +30,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_180135) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
