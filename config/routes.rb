6f32593de88be86639680ee98d11667bc72d3f56
Nyame-Wolf <mumenyam@gmail.com>
2022-11-15 10:14:07 +0300

    fix occurences routes to be categories children

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :occurrences
+  
   devise_for :users
   root "splash_screen#index"
-  resources :categories
+  resources :categories do
+    resources :occurrences
+  end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
