448107403ed664f9bb297cd837dca1a66e0b26da
Nyame-Wolf <mumenyam@gmail.com>
2022-11-16 00:41:04 +0300

    add foreign key to categories from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_100401) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_144136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_100401) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
   create_table "categories_occurrences", id: false, force: :cascade do |t|
@@ -53,5 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_100401) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users", column: "author_id"
   add_foreign_key "occurrences", "users", column: "author_id"
 end
