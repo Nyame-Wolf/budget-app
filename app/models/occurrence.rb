0fe750414a7f82ff3b20e88d87255ae61320501c
Nyame-Wolf <mumenyam@gmail.com>
2022-11-16 00:21:34 +0300

    fix models so that they accept author_id as foreign key

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,4 +1,4 @@
 class Occurrence < ApplicationRecord
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :user, foreign_key: 'author_id', class_name: 'User'
   has_and_belongs_to_many :categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :categories
-  has_many :occurrences
+  has_many :categories, foreign_key: 'author_id'
+  has_many :occurrences, foreign_key: 'author_id'
 
   validates :name, presence: true
 end
