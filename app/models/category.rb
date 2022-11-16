4ab5a7b7f1d02d0b683c7de7c153f93127b2a14a
Nyame-Wolf <mumenyam@gmail.com>
2022-11-16 23:45:17 +0300

    add validation to have unique categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id', class_name: 'User'
   has_and_belongs_to_many :occurrences
 
-  validates :name, :icon, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user }
+  validates :icon, presence: true
 end
