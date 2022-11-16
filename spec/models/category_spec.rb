f9cb64da52d18ae31cfa66f10f8cd46ac75a53b5
Nyame-Wolf <mumenyam@gmail.com>
2022-11-16 23:40:50 +0300

    add model tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(name: 'Mumenya Nyamu', email: 'success@example.com', password: 'password',
+                         password_confirmation: 'password')
+  end
+
+  let(:valid_attributes) do
+    {
+      name: 'Game',
+      icon: '🎲',
+      user: @user
+    }
+  end
+
+  subject { Category.new valid_attributes }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    subject.icon = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without an user' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'icon should return a string' do
+    expect(subject.icon).to be_a_kind_of(String)
+  end
 end
--- spec/models/occurrence_spec.rb
+++ spec/models/occurrence_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Occurrence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(name: 'Mumenya Nyamu', email: 'success@example.com', password: 'password',
+                         password_confirmation: 'password')
+    @category = Category.create!(name: 'Game', icon: '🎲', user: @user)
+  end
+
+  let(:valid_attributes) do
+    {
+      name: 'Football',
+      amount: 300,
+      user: @user,
+      categories: [@category]
+    }
+  end
+
+  subject { Occurrence.new valid_attributes }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without an user' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'check if amount is a number ' do
+    expect(subject.amount).to be_a_kind_of(Numeric)
+  end
 end
