37a4474d2caf69528e7adefbebd2b77224fd7773
Nyame-Wolf <mumenyam@gmail.com>
2022-11-16 22:41:22 +0300

    fix error with the occurence edit and destroy on the form

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,4 +1,7 @@
 class Occurrence < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id', class_name: 'User'
   has_and_belongs_to_many :categories
+
+  validates :name, :amount, presence: true
+  validates_presence_of :categories
 end
